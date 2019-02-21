5cb814d34f471851193c9d8c54089c270cc77c14
Rafael Requião <rafaelfrequiao@gmail.com>
2019-02-21 19:57:54 -0300

    implementando coisas legais e dando certo

--- pyAdventure-cliente.py
+++ pyAdventure-cliente.py
@@ -9,7 +9,7 @@
 import socket
 
 HOST = input("Digite o nome do host: ") #'127.0.0.1'
-PORT = 50123  # ADV0 em T9
+PORT = 50777  # ADV0 em T9
 
 tcp = socket.socket(
     socket.AF_INET , socket.SOCK_STREAM
@@ -23,9 +23,38 @@ print("HOST: " + HOST + ", PORT: " + str(PORT))
 tcp.connect(dest)
 print("Para sair use CTRL+X\n")
 
-msg = input()
+#msg = input() # estado inicial
+msg = "Olá!"
 
-while msg != '\x18':
-    tcp.send (msg.encode())
+print("Você disse '" + msg + "' para o servidor...")
+
+nomeJogador     = ""
+armaJogador     = ""
+
+
+while (msg != '\x18') or (msg != "/kick " + nomeJogador) :            # respeitar ordem correta de operações para não dar merda
+    # envia qualquer coisa
+    tcp.send(msg.encode())
+
+    data_received = tcp.recv(1024)
+    print(data_received.decode())
+
+    # transformar comandos em tuples/array com comando e argumento(s)
+
+    if "/playername "               in data_received.decode() :
+        nomeJogador = data_received.decode().replace("/playername ", "")
+
+    if "/playerweapon "             in data_received.decode() :
+        armaJogador = data_received.decode().replace("/playerweapon ", "")
+        
+    if "/kick " + nomeJogador       in data_received.decode() :
+        nomeJogador = data_received.decode().replace("/kick ", "")
+        print("Ah, não! Você foi kickado pelo servidor! :^)")
+        
+
+    #tcp.send (msg.encode())
     msg = input()
+    
+
 tcp.close()
+quit
--- pyAdventure-servidor.py
+++ pyAdventure-servidor.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python           # This is server.py file                                                          
 
+class Jogador:
+   'Classe base para todos os jogadores'
+   contaJogadores = 0
+
+   def __init__(self, nome, itemInicial):
+      self.nome = nome
+      self.itemInicial = itemInicial
+      Jogador.contaJogadores += 1
+   
+   def quantosJogadores(self):
+     print ("Jogadores totais: %d" % Jogador.contaJogadores)
+
+   def mostraJogador(self):
+      print ("Nome : ", self.nome,  ", Item inicial: ", self.itemInicial)
+
+
+
+# terminou classe Jogador
+
 from pynput.keyboard import Key, Listener
 
 def on_press(key):
@@ -30,39 +49,98 @@ def on_new_client(clientsocket,addr):
         data_received = clientsocket.recv(1024)
         if not data_received: break        
         #do some checks and if msg == someWeirdSignal: break:
-        print(addr, ' >> ', data_received)
+        print("Cliente: ", addr, ' >> ', data_received.decode())
         #msg = input('SERVER >> ')
         
         #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-        
-        #clientsocket.send(msg)
 
+        # iniciar novo objeto da classe Jogador
+
+        #firstMsg = clientsocket.recv(1024).decode()
+        #if firstMsg == "Olá!" :
+
+        msg = "Qual o seu nome?" + "\n"
+        clientsocket.send(msg.encode())
+
+        nomeConfirma = False
+
+        while nomeConfirma == False :
+            nome = clientsocket.recv(1024).decode()
+            msg = "Seu nome é: '" + nome + "' ?" + "\n"
+            clientsocket.send(msg.encode())
+
+            nomeConfirmacao = clientsocket.recv(1024).decode()
+            
+            if (nomeConfirmacao == "sim") :
+                msg = "/playername " + nome + "\n"
+                clientsocket.send(msg.encode())
+                nomeConfirma = True
+            
+        # talvez bugue
+
+        msg = "Escolha sua arma:\n    1 - pistola\n    2 - marreta\n    3 - chave inglesa\n    > Escolheu: "
+        clientsocket.send(msg.encode())
+        arma = clientsocket.recv(1024).decode()
+
+        if (arma=="1") or (arma=="2") or (arma=="3"):
+            #criar comando pra vincular arma ao jogador
+            msg = "/playerweapon " + arma + "\n"
+            clientsocket.send(msg.encode())
+            
+            msg = "Você escolheu " + arma + " ! Muito bem. Agora vamos..." + "\n"
+            clientsocket.send(msg.encode())
+
+            msg = "/kick " + nome + "\n"
+            clientsocket.send(msg.encode())
+
+            break        
+
+        else:
+            msg = "Tente novamente!"
+            clientsocket.send(msg.encode())
+            break
+
+
+        #else :
+        #    msg = "Por favor, cumprimente o servidor corretamente!"
+        #    clientsocket.send(msg.encode())
+
+    print("Finalizando conexão do cliente: ", addr)
     clientsocket.close()
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 50123                # Reserve a port for your service.
 
-print('Server started!')
-print("HOST: ", host, ":", str(port))
-print('Waiting for clients...')
+def Main():
 
-s.bind((host, port))        # Bind to the port
-s.listen(5)                 # Now wait for client connection.
+    
+    s = socket.socket()         # Create a socket object
+    host = '127.0.0.1'            #socket.gethostname() # Get local machine name
+    port = 50777                # Reserve a port for your service.
 
-#print('Got connection from', addr)
+    print('Server started!')
+    print("HOST: ", host, ":", str(port))
+    print('Waiting for clients...')
 
-print("Para sair use CTRL+X\n")
-msg = ""
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
 
-while msg != '\x18':
-   c, addr = s.accept()     # Establish connection with client.
-   _thread.start_new_thread(on_new_client,(c,addr))
-   
-   msg = input()
+    #print('Got connection from', addr)
 
-   #Note it's (addr,) not (addr) because second parameter is a tuple
-   #Edit: (c,addr)
-   #that's how you pass arguments to functions when creating new threads using thread module.
-   
-s.close()
+    print("Para sair use CTRL+X\n")
+    #msg = ""
+
+    while True: #msg != '\x18':
+        con, addr = s.accept()     # Establish connection with client.
+        _thread.start_new_thread(on_new_client,(con,addr))
+        
+        #msg = input()
+
+        #Note it's (addr,) not (addr) because second parameter is a tuple
+        #Edit: (c,addr)
+        #that's how you pass arguments to functions when creating new threads using thread module.
+        
+    s.close()
+
+
+if __name__ == '__main__': 
+    Main() 
+    quit
